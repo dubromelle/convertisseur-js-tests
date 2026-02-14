3451497a3490d460547f01ca6764883b4caf21de
dubromelle <jerome.dubromelle@ac-amiens.fr>
2026-02-14 10:52:48 +0100

    Erreur volontaire conversion

--- src/convertir.js
+++ src/convertir.js
@@ -1,5 +1,5 @@
 // --- Logique métier (Isolée du DOM) ---
 export function celsiusToFahrenheit(c) {
     if (typeof c !== 'number' || isNaN(c)) return NaN;
-    return (c * 9/5) + 32;
+    return (c * 8/5) + 32;
 }
