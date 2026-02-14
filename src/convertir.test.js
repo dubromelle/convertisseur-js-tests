2cbc5e5cf15436daf4d9676e2429f43699766394
dubromelle <jerome.dubromelle@ac-amiens.fr>
2026-02-14 10:23:36 +0100

    Ajout tests unitaires

---
+++ src/convertir.test.js
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { celsiusToFahrenheit } from './conversions'
+
+test('0°C doit donner 32°F', () => {
+  expect(celsiusToFahrenheit(0)).toBe(32)
+})
+
+test('100°C doit donner 212°F', () => {
+  expect(celsiusToFahrenheit(100)).toBe(212)
+})
